835f75bac1205d99e8d63b4ba79f8c56a4ea3034
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-12-26 09:37:09 +0100

    K00 0.506

--- eval/kitti.sh
+++ eval/kitti.sh
@@ -8,3 +8,10 @@ parallel -j3 --lb \
     --output-tum-path results/estim_{}.txt \
 ::: 00 01 02 03 04 05 06 07 08 09 10
 
+# Eval kitti metrics for each sequence alone:
+for d in 00 01 02 03 04 05 06 07 08 09 10 11; do
+  kitti-metrics-eval -r estim_${d}.txt -s ${d} --no-figures
+done
+
+# Eval overall kitti metrics:
+kitti-metrics-eval -r results/estim_%02i.txt -s 00 -s 01 -s 02 -s 03 -s 04 -s 05 -s 06 -s 07 -s 08 -s 09 -s 10 --no-figures
