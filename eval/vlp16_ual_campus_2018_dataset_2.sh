fc5e4a2b6e0c225ccd78515ee08ded9b7b6b4c65
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-12-29 11:58:47 +0100

    add more eval scripts

--- eval/vlp16_ual_campus_2018_dataset_2.sh
+++ eval/vlp16_ual_campus_2018_dataset_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MOLA_INPUT_RAWLOG=/mnt/storage/ual-datasets/ecarm_2018_02_23_velodyne_rtk_campus/2018-02-23_merged_lidar_only.rawlog \
+MOLA_INPUT_RAWLOG=/mnt/storage/ual-datasets/ecarm_2018_02_26_velodyne_rtk_campus/dataset_2018-02-26_merged.rawlog \
 MOLA_ODOMETRY_PIPELINE_YAML=$HOME/ros2_ws/src/mola_lidar_odometry/params/lidar-inertial-pipeline-simple.yaml \
-MOLA_TIME_WARP=0.1 \
+MOLA_TIME_WARP=2.0 \
 MOLA_LIDAR_NAME=Velodyne1_SCAN \
   mola-cli \
     -c $HOME/ros2_ws/src/mola_lidar_odometry/mola-cli-launchs/lidar_odometry_from_rawlog.yaml $@
