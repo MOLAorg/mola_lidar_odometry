06d12d346672432d31788ada2b89316d9244f0b9
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-01-28 13:08:51 +0100

    add dual map config

--- eval/cli_kitti.sh
+++ eval/cli_kitti.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Default pipeline YAML file:
 PIPELINE_YAML="${PIPELINE_YAML:-$SCRIPT_DIR/../params/lidar-odometry-pipeline-default.yaml}"
-SEQS_TO_RUN="${SEQS_TO_RUN:-00 01 02 03 04 05 06 07 08 09 10}"
+DEFAULT_SEQS_TO_RUN="00 01 02 03 04 05 06 07 08 09 10"
+SEQS_TO_RUN="${SEQS_TO_RUN:-${DEFAULT_SEQS_TO_RUN}}"
+NUM_THREADS=1
 
 if [ ! -f $PIPELINE_YAML ]; then
     echo "Error: Expected local file: '$PIPELINE_YAML'"
@@ -12,7 +14,7 @@ if [ ! -f $PIPELINE_YAML ]; then
     exit 1
 fi
 
-parallel -j2 --lb --halt now,fail=1 \
+parallel -j${NUM_THREADS} --lb --halt now,fail=1 \
   SEQ={} \
   MOLA_INITIAL_VX=18.0 \
   mola-lidar-odometry-cli \
@@ -29,4 +31,6 @@ for d in $SEQS_TO_RUN; do
 done
 
 # Eval overall kitti metrics:
-kitti-metrics-eval -r results/estim_%02i.txt -s 00 -s 01 -s 02 -s 03 -s 04 -s 05 -s 06 -s 07 -s 08 -s 09 -s 10 --no-figures
+if [ "$DEFAULT_SEQS_TO_RUN" = "$SEQS_TO_RUN" ]; then
+  kitti-metrics-eval -r results/estim_%02i.txt -s 00 -s 01 -s 02 -s 03 -s 04 -s 05 -s 06 -s 07 -s 08 -s 09 -s 10 --no-figures
+fi
