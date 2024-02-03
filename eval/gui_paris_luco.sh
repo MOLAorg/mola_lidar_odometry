988c73876b674113ba492cc56293c501345373d0
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2024-02-03 20:16:53 +0100

    more params and clean up

---
+++ eval/gui_paris_luco.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+PIPELINE_YAML="${PIPELINE_YAML:-$SCRIPT_DIR/../params/lidar-odometry-pipeline-default.yaml}"
+
+mola-cli \
+  $SCRIPT_DIR/../mola-cli-launchs/lidar_odometry_from_paris_luco.yaml \
+  $@
