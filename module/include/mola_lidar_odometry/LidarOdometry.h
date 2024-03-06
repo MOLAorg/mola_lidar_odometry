dcd4c40690ba95f750292ee20c19661220de1215
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2024-03-06 01:29:33 +0100

    refactor subclass into new package

--- module/include/mola_lidar_odometry/LidarOdometry.h
+++ module/include/mola_lidar_odometry/LidarOdometry.h
@@ -28,6 +28,7 @@
 #include <mola_kernel/interfaces/FrontEndBase.h>
 #include <mola_navstate_fuse/NavStateFuse.h>
 #include <mola_navstate_fuse/NavStateFuseParams.h>
+#include <mola_pose_list/SearchablePoseList.h>
 #include <mp2p_icp/ICP.h>
 #include <mp2p_icp/Parameterizable.h>
 #include <mp2p_icp_filters/FilterBase.h>
@@ -316,57 +317,6 @@ class LidarOdometry : public FrontEndBase
         mrpt::poses::CPose3DPDFGaussian found_pose_to_wrt_from;
     };
     void run_one_icp(const ICP_Input& in, ICP_Output& out);
-
-    class SearchablePoseList
-    {
-       public:
-        SearchablePoseList() = default;
-
-        SearchablePoseList(bool measure_from_last_kf_only)
-            : from_last_only_(measure_from_last_kf_only)
-        {
-        }
-
-        bool empty() const
-        {
-            if (from_last_only_)  //
-                return last_kf_ == mrpt::poses::CPose3D::Identity();
-            else
-                return kf_poses_.empty();
-        }
-
-        size_t size() const { return from_last_only_ ? 1 : kf_poses_.size(); }
-
-        void insert(const mrpt::poses::CPose3D& p)
-        {
-            if (from_last_only_)
-            {  //
-                last_kf_ = p;
-            }
-            else
-            {
-                kf_points_.insertPoint(p.translation());
-                kf_poses_.push_back(p);
-            }
-        }
-
-        [[nodiscard]] std::tuple<
-            bool /*isFirst*/, mrpt::poses::CPose3D /*distanceToClosest*/>
-            check(const mrpt::poses::CPose3D& p) const;
-
-        void removeAllFartherThan(
-            const mrpt::poses::CPose3D& p, const double maxTranslation);
-
-       private:
-        // if from_last_only_==true
-        mrpt::poses::CPose3D last_kf_ = mrpt::poses::CPose3D::Identity();
-
-        // if from_last_only_==false
-        std::deque<mrpt::poses::CPose3D> kf_poses_;
-        mrpt::maps::CSimplePointsMap     kf_points_;
-
-        bool from_last_only_ = false;
-    };
 
     /** All variables that hold the algorithm state */
     struct MethodState
