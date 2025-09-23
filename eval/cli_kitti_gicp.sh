dbff64d6b07e5ed388d9058dba1e02448849d1c0
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-09-23 18:49:00 +0200

    Expose more params from pipeline

--- eval/cli_kitti_gicp.sh
+++ eval/cli_kitti_gicp.sh
@@ -3,11 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Default pipeline YAML file:
-PIPELINE_YAML="$SCRIPT_DIR/../pipelines/lidar3d-ndt.yaml"
-#DEFAULT_SEQS_TO_RUN="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21"
-DEFAULT_SEQS_TO_RUN="00 01 02 03 04 05 06 07 08 09 10 11"
+PIPELINE_YAML="$SCRIPT_DIR/../pipelines/lidar3d-gicp.yaml"
+DEFAULT_SEQS_TO_RUN="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21"
 SEQS_TO_RUN="${SEQS_TO_RUN:-${DEFAULT_SEQS_TO_RUN}}"
-NUM_THREADS="${NUM_THREADS:-3}"
+NUM_THREADS="${MOLA_NUM_THREADS:-1}"
 
 if [ ! -f $PIPELINE_YAML ]; then
     echo "Error: Expected local file: '$PIPELINE_YAML'"
@@ -20,10 +19,11 @@ fi
 # for usage with the loop-closure postprocessing tool and afterwards
 # still be able to recover the full trajectory for metric error evaluation.
 
+#  MOLA_INITIAL_VX=18.0 \
+
 
 parallel -j${NUM_THREADS} --lb --halt now,fail=1 \
   SEQ={} \
-  MOLA_INITIAL_VX=18.0 \
   MOLA_SIMPLEMAP_ALSO_NON_KEYFRAMES=true \
   MOLA_SIMPLEMAP_MIN_XYZ=10.0 \
   MOLA_SIMPLEMAP_MIN_ROT=20.0 \
@@ -32,7 +32,8 @@ parallel -j${NUM_THREADS} --lb --halt now,fail=1 \
   mola-lidar-odometry-cli \
     -c $PIPELINE_YAML\
     --input-kitti-seq {} \
-    --output-tum-path results/kitti_{}_mola_ndt.tum \
+    --output-tum-path results/kitti_{}_mola_gicp.tum \
+    --verbosity DEBUG \
     $@ \
 ::: $SEQS_TO_RUN
 
@@ -41,11 +42,11 @@ parallel -j${NUM_THREADS} --lb --halt now,fail=1 \
 # Eval kitti metrics for each sequence alone:
 for d in $SEQS_TO_RUN; do
   if [ -f results/kitti_${d}_mola_gt.tum ]; then
-    kitti-metrics-eval -r results/kitti_${d}_mola_ndt.tum -s ${d} --no-figures
+    kitti-metrics-eval -r results/kitti_${d}_mola_gicp.tum -s ${d} --no-figures
   fi
 done
 
 # Eval overall kitti metrics:
 if [ "$DEFAULT_SEQS_TO_RUN" = "$SEQS_TO_RUN" ]; then
-  kitti-metrics-eval -r results/kitti_%02i_mola_ndt.tum -s 00 -s 01 -s 02 -s 03 -s 04 -s 05 -s 06 -s 07 -s 08 -s 09 -s 10 --no-figures
+  kitti-metrics-eval -r results/kitti_%02i_mola_gicp.tum -s 00 -s 01 -s 02 -s 03 -s 04 -s 05 -s 06 -s 07 -s 08 -s 09 -s 10 --no-figures
 fi
